d845c2233e5e3d5c474f7f95ab69d79a67ec04da
Luís Grós <luis.gros@olx.com>
2016-12-15 14:50:35 +0000

    fix entrypoint

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ pio-start-all
 
 jps -l
 
-pio app new misq
+pio app new misq --id 2 --access-key OVobAwW87UspbSN9jI0N6JCd80FPxpGM
 
-#python data/import_eventserver.py --access_key <your_access_key> --file data/train.tsv
+python data/import_eventserver.py --access_key OVobAwW87UspbSN9jI0N6JCd80FPxpGM --file data/train.tsv
 
-#pio build && pio train && pio deploy
+pio build && pio train && pio deploy
